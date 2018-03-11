2931102b8c69f2e73c88292d35d60543f0a84624
Ivan Dejanovic <idejanovic@gmail.com>
2018-03-11 23:04:03 +0100

    Removed fmt.

--- src/goWebServerBasic/goWebServerBasic.go
+++ src/goWebServerBasic/goWebServerBasic.go
@@ -1,19 +1,16 @@
 package main
 
 import (
-	"fmt"
 	"goWebServerBasic/router"
 	"net/http"
 )
 
 func main() {
-	fmt.Println("Started")
 	var addr string = ":8000"
 	var handler http.Handler = router.CreateRouteHandler()
 
 	// Bind to a port and pass route handler in
 	server := http.Server{Addr: addr, Handler: handler}
 
-	fmt.Println("Fire")
 	server.ListenAndServe()
 }
